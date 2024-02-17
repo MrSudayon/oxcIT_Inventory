995132b78340f4db492973c1ad37d71e9b3ad2f3
Suuuuu <sudayonfernando01@gmail.com>
2024-02-17 09:38:28 +0800

    asdasd

--- admin/remove.php
+++ admin/remove.php
@@ -4,16 +4,19 @@ include ('../php/db_connection.php');
 $db = new Connection();
 
 $id = $_GET['id'];
+$user_sess = $select->selectUserById($_SESSION['id']);
+$name = $user_sess['username'];
 
 $query = mysqli_query($db->conn, "UPDATE assets_tbl SET status='Archive' WHERE id='$id'");
 
-?>
-<script>
-    alert('Record Removed')
+$tag = mysqli_query($db->conn, "SELECT * FROM assets_tbl WHERE id = $id");
+while($row = $tag->fetch_assoc()) {
+    $assettag = $row['assettag'];
+}
+$sql = mysqli_query($db->conn, "INSERT INTO history_tbl (id, name, action, date)
+                        VALUES ('', '$name', 'Deleted Tag: $assettag from Assets Record', NOW())");
 
-</script>
-<?php
-    header("Location: dashboard.php");
+header("Location: dashboard.php");
 
 
 ?>
