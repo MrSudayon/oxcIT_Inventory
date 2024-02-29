4499542aa3934ea6ccdf806f3c94d70d9eceff94
Suuuuu <sudayonfernando01@gmail.com>
2024-02-29 09:16:11 +0800

    finishing

--- admin/configuration.php
+++ admin/configuration.php
@@ -21,46 +21,25 @@ if(!empty($_SESSION['id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../assets/logo.jpg">
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/config.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Configuration</title>
 </head>
-<style>
-.contents {
-    min-height: 100dvh;
-    background: linear-gradient(-95deg, var(--ter-color), var(--bg-color));
-    padding: 10px 40px;
-    overflow: hidden;
-}
-.config-buttons {
-    border: 1px solid black;
-    display: flex;
-    justify-content: space-evenly;
-}
-.config-btns img {
-    height: 200px;
-    width: auto;
-    
-}
-
-</style>
 <body>
     <?php include '../inc/header.php'; ?>
     
-        <div class="contents">
+        <div class="content">
             <div class="title">
                 <h1> Configuration Dashboard </h1>
             </div>
             <div class="config-buttons">
-                <div class="config-btns">
-                    <a href="../admin/emp_List.php" class="config-btn"><img src="../assets/icons/config/employee.png"></a>
-                    <a href="../admin/asset_List.php" class="config-btn"><img src="../assets/icons/config/asset.png"></a>
-                    <a href="../admin/dept_List.php" class="config-btn"><img src="../assets/icons/config/division.png"></a>
-                    <a href="../admin/location_List.php" class="config-btn"><img src="../assets/icons/config/location.png"></a>
-                </div>
+                <div class="config-btn"><a href="../admin/emp_List.php"><img src="../assets/icons/config/employee.png"></a></div>
+                <div class="config-btn"><a href="../admin/asset_List.php"><img src="../assets/icons/config/asset.png"></a></div>
+                <div class="config-btn"><a href="../admin/dept_List.php"><img src="../assets/icons/config/division.png"></a></div>
+                <div class="config-btn"><a href="../admin/location_List.php"><img src="../assets/icons/config/location.png"></a></div>
             </div>
         </div>
-
+        
         <script src="../js/dashboard.js"></script>
 </body>
 </html>
--- admin/emp_List.php
+++ admin/emp_List.php
@@ -45,10 +45,7 @@ if(!empty($_SESSION['id'])) {
                 <div class="link-btns">
                     <a href="../php/add_emp_info.php" class="link-btn">Add Emp</a>
                     <!--  -->
-                    <a href="../admin/emp_List.php" class="link-btn">Employee</a>
-                    <a href="../admin/asset_List.php" class="link-btn">Asset</a>
-                    <a href="../admin/dept_List.php" class="link-btn">Division</a>
-                    <a href="../admin/location_List.php" class="link-btn">Location</a>
+                    
                 </div>
 
                 <?php
