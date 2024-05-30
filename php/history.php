b08e8c32e9fb3e9b8f53f9c89d7bd038cf150bb2
Fernando Sudayon <sudayonfernando01@gmail.com>
2024-05-30 09:40:23 +0800

    history

--- php/history.php
+++ php/history.php
@@ -11,36 +11,21 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>History</title>
 </head>
-<body>
-    <header>
-        <div class="logo">
-            <a href="../admin/dashboard.php"><img src="../assets/logo.png" width="150px"></img></a>
-        </div>
-        <div class="dropdown">
-            <button class="dropbtn">Menu</button>
-            <div class="dropdown-content">
-                <a href="../php/history.php">History</a>
-                <a href="../php/logout.php?id=<?php echo $id; ?>&name=<?php echo $username; ?>">Logout</a>
-            </div>
-        </div>
-    </header>
+<?php include '../inc/header.php'; ?>
     
-        <div class="content">
-            <div class="title">
-                <h1> History </h1>
-                <div class="search-container">
-                <form action="" method="POST">
-                    <input type="text" placeholder="Search.." name="search">
-                    <button type="submit"><i class="fa fa-search"></i></button>
-                </form>
-                </div>
-            </div>
-
-        <div class="table-nav">
-            <div class="pagination-btns">
+<div class="content">
+    <div class="title">
+        <h1> History </h1>
+        <div class="search-container">
+        <form action="" method="POST">
+            <input type="text" placeholder="Search.." name="search">
+            <button type="submit"><i class="fa fa-search"></i></button>
+        </form>
+        </div>
+    </div>
+    <div class="table-nav">
+        <div class="pagination-btns">
             <?php
-
-
                 $sqlSelectAll = "SELECT * FROM history_tbl";
                 $results = mysqli_query($db->conn, $sqlSelectAll);
 
@@ -86,35 +71,34 @@
                         echo '<a href="history.php?page=' . ($page + 1) . '" class="next">Next</a>';
                     }
                 }
-                ?>
-            </div>
-            <div class="count">
-                <p>Asset count: <b style="color: yellow; font-size: 20px;"><?php echo $rowCountPage; ?></b></p>
-            </div>
-        </div>        
-                <table class="assets-table">
-                    <tr>
-                  
-                        <th>User</th>
-                        <th>Action</th>
-                        <th>Date</th>
-                    </tr>
-                    
-                    <tr>
-                    <?php
-                        while($row = mysqli_fetch_assoc($res)) {
-                    ?> 
-                        <td><?php echo $row['name']; ?></td>
-                        <td><?php echo $row['action']; ?></td>
-                        <td><?php echo $row['date']; ?></td>
-                    </tr>
-                    <?php
-                        }
-                    ?>
-                </table>
-            
+            ?>
         </div>
+        <div class="count">
+            <p>Asset count: <b style="color: yellow; font-size: 20px;"><?php echo $rowCountPage; ?></b></p>
+        </div>
+    </div>        
+    <table class="assets-table">
+        <tr>
+        
+            <th>User</th>
+            <th>Action</th>
+            <th>Date</th>
+        </tr>
+        
+        <tr>
+        <?php
+            while($row = mysqli_fetch_assoc($res)) {
+        ?> 
+            <td><?php echo $row['name']; ?></td>
+            <td><?php echo $row['action']; ?></td>
+            <td><?php echo $row['date']; ?></td>
+        </tr>
+        <?php
+            }
+        ?>
+    </table>
+</div>
 
-        <script src="../js/dashboard.js"></script>
+<script src="../js/dashboard.js"></script>
 </body>
 </html>
